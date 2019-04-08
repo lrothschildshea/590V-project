59ea8f47d67f40da1b65d8a88ae3cd33a074c8f6
Nicholas Perello <nap@1x-nat-vl930-172-30-120-84.wireless.umass.edu>
2019-04-08 13:34:15 -0400

    fixed to now deleted the not listed columns

--- columndelete.py
+++ columndelete.py
@@ -5,14 +5,16 @@ file = 'data/CCES16_Common_OUTPUT_Feb2018_VV.tab.tsv'
 print("Reading in ",  file)
 df = pd.read_csv(file, sep='\t')
 
-#Name of columns you wish to drop
-cols_to_drop = ['V101', 'commonweight_vv']
-if cols_to_drop:
-    print("Dropping the following columns: ", cols_to_drop)
-    for col in cols_to_drop:
-        df.drop(col, axis=1, inplace=True)
+headers = list(df)
+#Name of columns you wish to keep
+cols_to_keep = ['V101', 'commonweight_vv']
+if cols_to_keep:
+    print("Keeping the following columns: ", cols_to_keep)
+    for col in headers:
+        if col not in cols_to_keep:
+            df.drop(col, axis=1, inplace=True)
 else:
-    print("Didn't drop anything")
+    print("Not dropping everything")
 
 #print(df)
 
